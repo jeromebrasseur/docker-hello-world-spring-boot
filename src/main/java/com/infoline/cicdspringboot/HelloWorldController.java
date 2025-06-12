485b874db91e700c8e86f5dcd6fcb5c0f5ab563e
Jérôme BRASSEUR <raxiocool@gmail.com>
2025-06-12 09:43:06 +0200

    Update HelloWorldController.java

--- src/main/java/com/infoline/cicdspringboot/HelloWorldController.java
+++ src/main/java/com/infoline/cicdspringboot/HelloWorldController.java
@@ -1,5 +1,14 @@
 package com.infoline.cicdspringboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
 
+  @GetMapping("/")
+  public String hello(@RequestParam(name="nom", required = false, defaultvalue = "Jérôme")String nom) {
+    return "Salut " + nom + ", bienvenue au Spring-Boot CI/CD Infoline";
+  }
 }
