d580915e97a1d2578315dc93c0b849456ea6607c
Jérôme BRASSEUR <raxiocool@gmail.com>
2025-06-12 09:18:39 +0200

    Add files via upload

---
+++ src/test/java/com/infoline/cicdspringboot/CicdspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoline.cicdspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
