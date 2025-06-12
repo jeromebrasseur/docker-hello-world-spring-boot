677c1a9c0ebf2dc21c9169fb738630d0f3a7ebbd
Jérôme BRASSEUR <raxiocool@gmail.com>
2025-06-12 09:16:44 +0200

    Add files via upload

---
+++ src/main/java/com/infoline/cicdspringboot/CicdspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.infoline.cicdspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdspringbootApplication.class, args);
+	}
+
+}
